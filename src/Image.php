b6dabf4d304555c257894bf2665ae0d1b382aecc
jola stolpa <jstolpa@op.pl>
2017-01-03 20:57:44 +0100

    wiecej klasa image

--- src/Image.php
+++ src/Image.php
@@ -25,26 +25,109 @@ class Image {
         
     } 
     
-    function getId() {
+    public function getId() {
         return $this->id;
     }
-    function getImageLink() {
+    public function getImageLink() {
         return $this->imageLink;
     }
-    function getProductId() {
+    public function getProductId() {
         return $this->productId;
     }
    
-    function setId($id) {
+    public function setId($id) {
         $this->id = $id;
     }
-    function setImageLink($imageLink) {
+    public function setImageLink($imageLink) {
         $this->imageLink = $imageLink;
     }
-    function setProductId($productId) {
+    public function setProductId($productId) {
         $this->productId = $productId;
     }
-     
+    
+    public function saveToDb(mysqli $conn) { 
+        
+        if($this->id == -1){
+        
+            $sql = "INSERT INTO Images(id, imageLink, productId)"
+                . "VALUES ($this->id, '$this->imageLink','$this->productId,)";
+                    
+            $result = $conn->query($sql);
+            if($result == true){
+                
+                $this->id = $conn->insert_id;
+                return true;
+            } 
+        }else{ 
+            
+            $sql="UPDATE Images SET imageLink='$this->imageLink', "
+                    . "productId='$this->productId WHERE id='$this->id'";
+            
+            $result = $conn->query($sql);
+            if($result == true){              
+                return true;
+            }
+        }
+        return false;
+    } 
+      
+    public function delete(mysqli $conn){
+        
+        if($this->id != -1){
+            
+            $sql = "DELETE FROM Images WHERE id='$this->id'";
+            
+            $result = $conn->query($sql);
+            if($result == true){
+                $this->id = -1;
+                return true;
+            }
+            return false;
+        } 
+        return true; 
+    }
+ 
+    static public function loadImageById(mysqli $conn, $id){
+        
+        $sql = "SELECT * FROM Images WHERE id='$id'";
+        
+        $result = $conn->query($sql); 
+    
+        if($result == true && $result->num_rows == 1){
+            
+            $row = $result->fetch_assoc();
+            $loadedImage = new Image();
+            $loadedImage->id = $row['id'];
+            $loadedImage->imageLink = $row['imageLink'];
+            $loadedImage->productId = $row['productId'];
+            
+            return $loadedUser;
+        }
+        return null; 
+    }
+    
+    static public function loadAllImagesByProductId (mysqli $conn, $productId) { 
+        if($this->id != -1){
+            
+            $sql="SELECT Images.imageLink FROM Products JOIN Images ON 
+                Products.id=Pictures.productId WHERE Products.id=$productId ";
+            
+            $ret=[]; 
+            $result = $conn->query($sql);
+            if($result == true && $result->num_rows != 0){
+            
+            foreach($result as $row){
+                $loadedImage = new Image();
+                $loadedImage->imageLink = $row['imageLink'];
+              
+                
+                $ret[] = $loadedImage;
+            }
+        }
+        return $ret;
+        }
+    }
+            
 }
 
     
